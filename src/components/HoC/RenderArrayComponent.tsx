b518203777e0aee1d98f1019689e4a3869580372
dodomew <dodomew@gmail.com>
2020-12-30 19:53:27 +0100

    * added comments explaining the workings of the HoC used in rendering the title and subtitle of the projectItem

--- src/components/HoC/RenderArrayComponent.tsx
+++ src/components/HoC/RenderArrayComponent.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+/*
+    This HoC wants a React component as parameter and returns the same React component
+    if it matches a certain condition.
+
+    In this case, this component must have a prop which is atleast an Object, which should not be empty.
+    Note: Arrays are also Objects
+
+    Usage is primarily for Titles and Subtitles
+*/
+
 const withExistsRenderer = <T extends object>( WrappedComponent: React.FC<T> ): React.FC<T> => 
     ( props : T ) => 
     {
-        //console.log(props)
         if(Object.keys(props).length !== 0) {
             return <WrappedComponent {...props as T } />;
         }
--- src/components/Projects/ProjectItem/ProjectItem.tsx
+++ src/components/Projects/ProjectItem/ProjectItem.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
-import ProjectItemTitle from "./ProjectItemTitle";
 import withExistsRenderer from "../../HoC/RenderArrayComponent"
 
+/*
+    Prismic API sends a normal text input as follows:
+
+    key: [
+        {
+            key: value
+        }
+    ]
+
+    To check if this object is valid, we put it through a generic HoC which takes a generic <T> that extends an object
+    Because we have to type it like the Prismic API, we have to type it in 2 steps: first the key/value, then the wrapper
+    or container it is in, in this case an Array. Therefor you get TextPropArray, which is an Array containing TextProps
+
+    We then say to the components Title and Subtitle that their expected prop input is of type TextPropArray
+    We also pass the object/array containing the key/value as a prop to the HoC which checks validity
+
+    If it's all good, the HoC returns the original component and we can render it without doing extra checks
+*/
 interface TextProp
 {
     text : string
@@ -41,8 +58,6 @@ const ProjectItem = (data: ProjectItemProps) => {
     if(!data) {
         return null;
     }
-
-    // console.log("data", JSON.stringify(data))
 
     const { title, subtitle, link } = { ...data};
     const { url, dimensions } = { ...data.media};
