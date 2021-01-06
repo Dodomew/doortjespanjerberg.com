dac2111a201f00dc6e9120a33033e8db15cb833c
dodomew <dodomew@gmail.com>
2021-01-06 15:05:56 +0100

    * smol refactor container class, added a wrapper class for max width and padding

--- src/App.tsx
+++ src/App.tsx
@@ -23,26 +23,28 @@ const App = () => {
 		<Fragment>
 			<BrowserRouter>
 				<div className="rainbows"></div>
-				<Header />
-				<div className="content container">
-					<Route render={({ location }) => (
-						<SwitchTransition mode="out-in">
-							<CSSTransition
-								key={location.key}
-								classNames="fade"
-								unmountOnExit
-								timeout={166}>
-								<div className="anim">
-									<Switch location={location}>
-										<Route exact path="/" component={Homepage} />
-										<Route exact path="/projects/:uid" component={ProjectDetail} />
-										<Route exact path="/projects" component={Homepage} />
-									</Switch>
-								</div>
-							</CSSTransition>
-						</SwitchTransition>
-					)}
-					/>
+				<div className="wrapper">
+					<Header />
+					<div className="content container">
+						<Route render={({ location }) => (
+							<SwitchTransition mode="out-in">
+								<CSSTransition
+									key={location.key}
+									classNames="fade"
+									unmountOnExit
+									timeout={166}>
+									<div className="anim">
+										<Switch location={location}>
+											<Route exact path="/" component={Homepage} />
+											<Route exact path="/projects/:uid" component={ProjectDetail} />
+											<Route exact path="/projects" component={Homepage} />
+										</Switch>
+									</div>
+								</CSSTransition>
+							</SwitchTransition>
+						)}
+						/>
+					</div>
 				</div>
 			</BrowserRouter>
 		</Fragment>
